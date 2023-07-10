c70edd39d0cf2e6808465f0099c614c912d30e0a
shiracohen1 <shira050104@gmail.com>
2023-07-10 15:45:18 +0300

    heroo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './components/card'
 import Navbar from './components/navbar'
+import Hero from './components/hero'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Hero />
         <Card />
       </header>
     </div>
