c4991aff20a63148d1c9c5e189a3c29970d03002
shiracohen1 <shira050104@gmail.com>
2023-07-18 17:21:23 +0300

    idk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import Woman from "./images/woman.svg"
 import './App.css';
 import Card from './components/card'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
+import Data from './data'
 
 function App() {
+  const cards = Data.map(item => {
+    return (
+      <Card
+        key={ item.id }
+        item={ item }
+      />
+    )
+  })
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
         <Hero />
-        <Card
-          img={ Woman }
-          rating="5.0"
-          reviewCount={6}
-          location="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
+        <section className="cards-list">
+                {cards}
+            </section>
       </header>
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,30 @@
 import React from "react"
 import Star from "../images/star.svg"
+import Woman from '../images/woman.svg'
+import Woman2 from '../images/woman2.svg'
+import Bike from '../images/bike.svg'
 
-export default function Card(props) {
+export default function Card(item) {
+    const currItem = item.item;
+
+    let badgeText
+    if (currItem.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (currItem.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
-            <img src={props.img} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={currItem.coverImg} className="card--image" />
             <div className="card--stats">
                 <img src={ Star } className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{currItem.stats.rating}</span>
+                <span className="gray">({currItem.stats.reviewCount}) • </span>
+                <span className="gray">{currItem.location}</span>
             </div>
-            <p className="title">{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="title">{currItem.title}</p>
+            <p><span className="bold">From ${currItem.price}</span> / person</p>
         </div>
     )
 }
